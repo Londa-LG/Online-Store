04f6a06f59726970af5e8dc4cca424d0548e9b2e
Londa-LG <66629166+Londa-LG@users.noreply.github.com>
2020-07-01 13:01:26 +0200

    final product

--- Products/models.py
+++ Products/models.py
@@ -14,6 +14,7 @@ class Review(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
+    pic = models.ImageField(default='default.jpg',upload_to= 'product_pics')
     description = models.TextField()
     price = models.DecimalField(max_digits=100, decimal_places=2)
     in_stock = models.DecimalField(max_digits=100,decimal_places=0)
--- Products/views.py
+++ Products/views.py
@@ -3,7 +3,7 @@ from .models import Product , Review, About
 
 # Create your views here.
 def home(request):
-    first = Product.objects.exclude(id=4).exclude(id=5).exclude(id=6).exclude(id=7).exclude(id=8).exclude(id=9)
+    first = Product.objects.exclude(id=2).exclude(id=3).exclude(id=9).exclude(id=10).exclude(id=6).exclude(id=7)
     second = Review.objects.all()
     content = {"products01": first,
                "reviews": second,
@@ -18,9 +18,9 @@ def about(request):
     return render(request,'Products/about.html',{"about": About.objects.all})
 
 def product(request):
-    pro01= Product.objects.exclude(id=4).exclude(id=5).exclude(id=6).exclude(id=7).exclude(id=8).exclude(id=9)
-    pro02 = Product.objects.exclude(id=1).exclude(id=2).exclude(id=3).exclude(id=7).exclude(id=8).exclude(id=9)
-    pro03 = Product.objects.exclude(id=1).exclude(id=2).exclude(id=3).exclude(id=4).exclude(id=5).exclude(id=6)
+    pro01= Product.objects.exclude(id=4).exclude(id=5).exclude(id=7).exclude(id=8).exclude(id=9).exclude(id=10)
+    pro02 = Product.objects.exclude(id=2).exclude(id=3).exclude(id=6).exclude(id=8).exclude(id=9).exclude(id=10)
+    pro03 = Product.objects.exclude(id=2).exclude(id=3).exclude(id=4).exclude(id=5).exclude(id=6).exclude(id=7)
     content={ "products01": pro01,
               "products02": pro02,
               "products03":pro03
