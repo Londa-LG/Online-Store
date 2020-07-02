deaf90a4fa04489a3c6abc032b0046f7c27778fa
Londa-LG <66629166+Londa-LG@users.noreply.github.com>
2020-07-02 14:36:42 +0200

    commit

--- Products/models.py
+++ Products/models.py
@@ -8,9 +8,15 @@ class About(models.Model):
     faq_title = models.CharField(max_length=100,default="FAQ")
     faq = models.TextField()
 
+    def __str__(self):
+        return self.title
+
 class Review(models.Model):
     name = models.CharField(max_length=100)
     review = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
@@ -18,6 +24,9 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=100, decimal_places=2)
     in_stock = models.DecimalField(max_digits=100,decimal_places=0)
+
+    def __str__(self):
+        return self.title
 
     def get_absolute_url(self):
         return f"/products/{self.id}"
